8fcf81a00de2f75f391bbdaf989853277929d8e1
Joshua Akande <jaakande@sidmach.com>
2020-06-21 21:25:21 +0100

    Admin creation api

--- controllers/user.js
+++ controllers/user.js
@@ -135,6 +135,74 @@ exports.vendorSignup = async (req, res) => {
     res.status(500).send("Error in Saving");
   }
 };
+// Vendor Signup
+exports.adminSignup = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  const {name, username, email, password, gender, passcode} = req.body;
+  try {
+    let checkemail = await UserModel.findOne({
+      email,
+    });
+    let checkusername = await UserModel.findOne({
+      username,
+    });
+    if (checkemail || checkusername) {
+      return res.status(400).json({
+        msg: "Admin Already Exists",
+      });
+    }
+    if(passcode !== "root") {
+      return res.status(401).json({
+        msg: "unauthorized",
+      });
+    }
+    user = new UserModel({
+      name,
+      username,
+      email,
+      password,
+      gender,
+      role: "admin",
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user._id,
+        role: user.role,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      "randomString",
+      {
+        expiresIn: 10000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        return res.status(201).json({
+          token,
+          role: user.role,
+          message: "admin created Successfully",
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Error in Saving");
+  }
+};
 
 // User Login
 exports.login = async (req, res, next) => {
